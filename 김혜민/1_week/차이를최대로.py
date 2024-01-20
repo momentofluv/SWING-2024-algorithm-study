9db671f4f84333ff420c25655f6dab3577f3eb70
hm <panmahm@naver.com>
2024-01-20 19:29:01 +0900

    [Baekjoon-10819] 차이를 최대로

---
+++ 김혜민/1_week/차이를최대로.py
@@ -0,0 +1,15 @@
+import sys
+from itertools import permutations
+
+n = int(sys.stdin.readline())
+graph = list(map(int, sys.stdin.readline().split()))
+
+max_ans = 0
+
+for i in permutations(graph):
+    ans = 0
+    for j in range(n-1):
+        ans += abs(i[j] - i[j+1])
+    max_ans = max(max_ans, ans)
+
+print(max_ans)
